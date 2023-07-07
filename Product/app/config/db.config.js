6d0a0f5234e074ccb17672ccfab03c1b5514ee9a
selenechhangur <selenechhangur@gmail.com>
2023-07-07 14:02:39 -0400

    first commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+  //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+  url: "mongodb+srv://selenechhangur:tgx4pq5lFgkBINW6@cluster0.48qbylw.mongodb.net/DressStore?retryWrites=true&w=majority",
+};
